6e9c31a9ad80f5e73d22b76900b89f713ad4546d
André <AndreR@outlook.de>
2021-10-07 11:38:55 +0200

    MsTest mit DataRows

--- Calculator/Calculator.Tests/CalcTests.cs
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace Calculator.Tests
 {
@@ -16,6 +17,41 @@ namespace Calculator.Tests
 
             //Assert
             Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void Sum_0_and_0_results_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Sum_MAX_and_1_throws_OverflowException()
+        {
+            Calc calc = new Calc();
+
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [TestMethod]
+        [DataRow(0, 0, 0)]
+        [DataRow(1, 2, 3)]
+        [DataRow(-3, -2, -5)]
+        [DataRow(-3, 5, 2)]
+        public void Sum_DataRows(int a, int b, int expected)
+        {
+            Calc calc = new Calc();
+
+            var result = calc.Sum(a, b);
+            
+            Assert.AreEqual(expected, result);
         }
     }
 }
