718e84761b8d7472d92350e54672a48a4c575c1c
André <AndreR@outlook.de>
2021-10-08 10:26:04 +0200

    EfCore für GMV

---
+++ ppedv.GMV/ppedv.GMV.Data.EfCore/EfContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ppedv.GMV.Model;
+
+namespace ppedv.GMV.Data.EfCore
+{
+    public class EfContext : DbContext
+    {
+        public DbSet<Gerät> Geräte { get; set; }
+        public DbSet<Messwert> Messwerte { get; set; }
+        public DbSet<Messung> Messungen { get; set; }
+
+
+        private string _conString;
+
+        public EfContext(string conString)
+        {
+            _conString = conString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer(_conString).UseLazyLoadingProxies();
+        }
+    }
+}
