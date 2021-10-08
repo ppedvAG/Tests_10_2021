6d38f73931d97b1ae8c6710c02a8863dbe346853
André <AndreR@outlook.de>
2021-10-08 13:50:00 +0200

    Erste tests für die Logic

---
+++ ppedv.GMV/ppedv.GMV.Logic.Tests/CoreTests.cs
@@ -0,0 +1,50 @@
+using FluentAssertions;
+using ppedv.GMV.Model;
+using System;
+using Xunit;
+
+namespace ppedv.GMV.Logic.Tests
+{
+    public class CoreTests
+    {
+        [Fact]
+        public void GetMesswerteOfGerät_gerät_with_2_messungen_with_3_Werten_results_6()
+        {
+            var g = new Gerät();
+            var m1 = new Messung();
+            m1.Messwerte.Add(new Messwert());
+            m1.Messwerte.Add(new Messwert());
+            m1.Messwerte.Add(new Messwert());
+            var m2 = new Messung();
+            m2.Messwerte.Add(new Messwert());
+            m2.Messwerte.Add(new Messwert());
+            m2.Messwerte.Add(new Messwert());
+            g.Messungen.Add(m1);
+            g.Messungen.Add(m2);
+
+            var core = new Core();
+            var result = core.GetMesswerteOfGerät(g);
+
+            result.Should().HaveCount(6);
+        }
+
+        [Fact]
+        public void GetMesswerteOfGerät_gerät_NULL_should_thow_ArgumentNullException()
+        {
+            var core = new Core();
+            var act = () => core.GetMesswerteOfGerät(null);
+
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GetGerätWithHighestMesswert__()
+        {
+            var core = new Core();
+            var result =  core.GetGerätWithHighestMesswert();
+
+            result.Should().NotBeNull();
+        }
+
+    }
+}
---
+++ ppedv.GMV/ppedv.GMV.Logic/Core.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.GMV.Data.EfCore;
+using ppedv.GMV.Model;
+
+namespace ppedv.GMV.Logic
+{
+    public class Core
+    {
+        public IEnumerable<Messwert> GetMesswerteOfGerät(Gerät gerät)
+        {
+            if (gerät == null)
+                throw new ArgumentNullException(nameof(gerät));
+
+            return gerät.Messungen.SelectMany(X => X.Messwerte)
+                                  .OrderBy(x => x.Wert);
+        }
+
+        public Gerät GetGerätWithHighestMesswert()
+        {
+            var con = new EfContext("Server=(localdb)\\mssqllocaldb;Database=GMV;Trusted_Connection=true");
+            return con.Messwerte.OrderBy(x => x.Wert)
+                                .FirstOrDefault()
+                                .Messung.Gerät;
+        }
+
+    }
+}
